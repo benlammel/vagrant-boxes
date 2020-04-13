71946426ba5546ca68c02ade6afb54cb8d15b749
Ben <benjamin.lammel@lammel.ch>
2020-04-13 03:22:26 +0200

    update

--- vagrant-fuseki/bootstrap.sh
+++ vagrant-fuseki/bootstrap.sh
@@ -5,17 +5,11 @@ export LANGUAGE=en_US.UTF-8; export LANG=en_US.UTF-8; export LC_ALL=en_US.UTF-8;
 apt update
 apt upgrade -y
 
-apt-get -y install software-properties-common gnupg
+apt-get -y install openjdk-11-jdk
 
-wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
-add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
-apt update
-apt install -y adoptopenjdk-11-hotspot
-
-
-
-wget https://www-eu.apache.org/dist/jena/binaries/apache-jena-fuseki-3.13.1.tar.gz -O /opt/apache-fuseki.tar.gz
-tar -xvzf /opt/apache-fuseki.tar.gz -C /opt/
-mv /opt/apache-jena-fuseki* /opt/fuseki
-mkdir /opt/fuseki/run
-cp /vagrant/config/shiro.ini /opt/fuseki/run/shiro.ini
+#sudo -s
+#wget http://mirror.easyname.ch/apache/jena/binaries/apache-jena-fuseki-3.14.0.tar.gz -O /opt/apache-fuseki.tar.gz
+#tar -xvzf /opt/apache-fuseki.tar.gz -C /opt/
+#mkdir /opt/fuseki/run
+#cp /vagrant/config/shiro.ini /opt/fuseki/run/run/shiro.ini
+#sudo sh /opt/fuseki/fuseki-server
--- vagrant-quarkus-dev/bootstrap.sh
+++ vagrant-quarkus-dev/bootstrap.sh
@@ -3,16 +3,4 @@
 sudo apt-get update
 sudo apt upgrade -y
 
-sudo apt-get -y install software-properties-common maven gnupg
-
-wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
-add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
-sudo apt update
-apt install -y adoptopenjdk-11-hotspot
-
-
-#apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
-#curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-#add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-#apt-get update
-#apt-get install docker-ce -y
+apt-get -y install openjdk-11-jdk
