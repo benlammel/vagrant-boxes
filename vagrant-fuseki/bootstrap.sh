c312d7b5a8d6cbfbccdbe2a48cbb433c190115ee
Ben <benjamin.lammel@lammel.ch>
2020-04-13 03:38:48 +0200

    update

--- vagrant-fuseki/bootstrap.sh
+++ vagrant-fuseki/bootstrap.sh
@@ -10,6 +10,7 @@ apt-get -y install openjdk-11-jdk
 #sudo -s
 #wget http://mirror.easyname.ch/apache/jena/binaries/apache-jena-fuseki-3.14.0.tar.gz -O /opt/apache-fuseki.tar.gz
 #tar -xvzf /opt/apache-fuseki.tar.gz -C /opt/
-#mkdir /opt/fuseki/run
-#cp /vagrant/config/shiro.ini /opt/fuseki/run/run/shiro.ini
+#mv /opt/apache-jena-fuseki* /opt/fuseki
+#mkdir /opt/fuseki/run/
+#cp /vagrant/config/shiro.ini /opt/fuseki/run/shiro.ini
 #sudo sh /opt/fuseki/fuseki-server
