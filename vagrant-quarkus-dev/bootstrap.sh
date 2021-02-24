79123758e1620f2e383535c142609b9803df42b8
benlammel <benlammel@users.noreply.github.com>
2021-02-24 07:32:55 +0100

    Update bootstrap.sh

--- vagrant-quarkus-dev/bootstrap.sh
+++ vagrant-quarkus-dev/bootstrap.sh
@@ -2,5 +2,10 @@
 
 sudo apt-get update
 sudo apt upgrade -y
+sudo apt-get -y install openjdk-15-jdk maven
 
-apt-get -y install openjdk-11-jdk
+
+
+######
+#sudo apt-get install software-properties-common -y
+#sudo add-apt-repository ppa:openjdk-r/ppa -y
