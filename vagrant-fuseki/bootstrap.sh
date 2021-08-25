7c358222de4b5c3789bd1efb496300608b5db43f
benlammel <benlammel@users.noreply.github.com>
2021-08-25 04:58:58 +0200

    update

--- vagrant-fuseki/bootstrap.sh
+++ vagrant-fuseki/bootstrap.sh
@@ -4,11 +4,12 @@ export LANGUAGE=en_US.UTF-8; export LANG=en_US.UTF-8; export LC_ALL=en_US.UTF-8;
 
 apt update
 apt upgrade -y
+sudo apt-get -y install openjdk-17-jdk
 
 #apt-get -y install ntp openjdk-11-jdk
 
 #sudo -s
-#wget https://downloads.apache.org/jena/binaries/apache-jena-fuseki-4.0.0.tar.gz -O /opt/apache-fuseki.tar.gz
+#wget https://downloads.apache.org/jena/binaries/apache-jena-fuseki-4.1.0.tar.gz -O /opt/apache-fuseki.tar.gz
 #tar -xvzf /opt/apache-fuseki.tar.gz -C /opt/
 #mv /opt/apache-jena-fuseki* /opt/fuseki
 #mkdir /opt/fuseki/run/
