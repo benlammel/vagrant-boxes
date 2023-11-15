f89c8a76b566d4c3885b642c0bc62c26314a4d9f
benlammel <benlammel@users.noreply.github.com>
2023-11-15 04:33:00 +0100

    update

--- vagrant-postgresql/bootstrap.sh
+++ vagrant-postgresql/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export LANGUAGE=en_US.UTF-8; export LANG=en_US.UTF-8; export LC_ALL=en_US.UTF-8; locale-gen en_US.UTF-8
+#export LANGUAGE=en_US.UTF-8; export LANG=en_US.UTF-8; export LC_ALL=en_US.UTF-8; locale-gen en_US.UTF-8
 
-apt update
-apt upgrade -y
+pacman -S postgresql  jre-openjdk-headless wget --noconfirm
 
+#initdb -D /data
 
 #sudo apt install postgresql postgresql-contrib
